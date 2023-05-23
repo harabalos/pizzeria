08a14b4c32e7235930424a0972750b40e51c29b4
harabalos <babismetaxas000@gmail.com>
2023-05-23 12:41:29 +0300

    Initial Commit

---
+++ pizzeria/pizza.h
@@ -0,0 +1,26 @@
+#ifndef pizza_h
+#define pizza_h
+
+#define Ncook 10
+#define Noven 15
+#define Npacker 2
+#define Ndeliverer 10
+#define Torderlow 1
+#define Torderhigh 3
+#define Norderlow 1
+#define Norderhigh 5
+#define Pplain 0.6
+#define Tpaymentlow 1
+#define Tpaymenthigh 3
+#define Pfail 0.1
+#define Cplain 10
+#define Cspecial 12
+#define Tprep 1
+#define Tbake 10
+#define Tpack 1
+#define Tdellow 5
+#define Tdelhigh 15
+
+
+#endif
+
