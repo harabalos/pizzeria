d1404e63f0e0d25b70ca7cbe87c0a6512339b57f
harabalos <babismetaxas000@gmail.com>
2023-05-25 20:00:38 +0300

    deleted file and made order more effective

--- pizzeria/main.c
+++ pizzeria/main.c
@@ -6,38 +6,6 @@
 #include <time.h>
 
 
-int Ncook = 10;
-int Noven = 15;
-int Npacker = 2;
-int Ndeliverer = 10;
-int Torderlow = 1;
-int Torderhigh = 3;
-int Norderlow = 1;
-int Norderhigh = 5;
-double Pplain = 0.6;
-int Tpaymentlow = 1;
-int Tpaymenthigh = 3;
-double Pfail = 0.1;
-int Cplain = 10;
-int Cspecial = 12;
-int Tprep = 1;
-int Tbake = 10;
-int Tpack = 1;
-int Tdellow = 5;
-int Tdelhigh = 15;
-int profit = 0;
-int countPlain = 0;
-int countSpecial = 0;
-int countSuccess = 0;
-int countFail = 0;
-long coolingTime = 0;
-long sumTime = 0;
-long maxTime = 0;
-long maxTimeCooling = 0;
-long avgTime = 0;
-long avgTimeCooling = 0;
-
-
 int main(int argc, char *argv[])  {
 
     if (argc != 3) {
@@ -60,7 +28,7 @@ int main(int argc, char *argv[])  {
         id[i] = i+1;
         printf("Customer %d is ordering.\n", i+1);
         rc = pthread_create(&threads[i], NULL, order, &id[i]);
-        int nextCustomer = rand_r(&seed) % Torderhigh + Torderlow;
+        int nextCustomer = rand_r(&seed) % (Torderhigh - Torderlow + 1) + Torderlow;
         sleep(nextCustomer);
     }
 
@@ -76,4 +44,148 @@ int main(int argc, char *argv[])  {
 
     return 0;
 
+}
+
+void *order(void *x){
+
+    int id = *(int *)x;
+    printf("Order %d started\n", id);
+    
+    counterOrder++;
+    
+    int isLastOrder = 0;
+    if (id == Ncust){
+        isLastOrder = 1;
+    }
+    
+    int numberOfPizzas = rand_r(&seed) % (Norderhigh - Norderlow +1) + Norderlow;
+    int pizzas[numberOfPizzas];
+    int paymentTime = rand_r(&seed) % (Tpaymenthigh - Tpaymentlow + 1) + Tpaymentlow;
+    double acceptedCard = (double)rand_r(&seed) / RAND_MAX;
+    sleep(paymentTime);
+    
+    if(acceptedCard > Pfail){
+        acceptOrder(id,numberOfPizzas,&pizzas[numberOfPizzas]);
+    }else{
+        declineOrder(id);
+    }
+     
+    rc = pthread_mutex_lock(&lock);
+    while (Ncook == 0) {
+        printf("Order %d did not find available cook. Blocked...\n", id);
+        rc = pthread_cond_wait(&cond, &lock);
+    }
+    printf("Order %d is being prepared.\n", id);
+    Ncook--;
+    rc = pthread_mutex_unlock(&lock);
+
+    sleep(Tprep);
+
+    rc = pthread_mutex_lock(&lock);
+    while (Noven < numberOfPizzas) {
+        printf("Order %d did not find available ovens for all the pizzas at the same time. Blocked...\n", id);
+        rc = pthread_cond_wait(&cond, &lock);
+    }
+    printf("Order %d is cooking.\n", id);
+    Noven-=numberOfPizzas;
+    rc = pthread_mutex_unlock(&lock);
+    
+    Ncook++;
+    sleep(Tbake);
+    clock_gettime(CLOCK_REALTIME,&start_time_cooling);
+    
+    rc = pthread_mutex_lock(&lock);
+    while (Npacker == 0) {
+        printf("Order %d did not find available packer. Blocked...\n", id);
+        rc = pthread_cond_wait(&cond, &lock);
+    }
+    Npacker--;
+    rc = pthread_mutex_unlock(&lock);
+
+    sleep(Tpack * numberOfPizzas);
+    //X time
+    clock_gettime(CLOCK_REALTIME,&end_timeX);
+    secondsX = (int)(end_timeX.tv_sec - start_time.tv_sec);
+    printf("Order %d has been prepared in %d minutes\n",id,secondsX);
+    Npacker++;
+    Noven+=numberOfPizzas;
+
+    
+    int deliveryTime = rand_r(&seed) % (Tdelhigh - Tdellow + 1) + Tdellow;
+    rc = pthread_mutex_lock(&lock);
+    while (Ndeliverer == 0) {
+        printf("Order %d did not find available deliverer. Blocked...\n", id);
+        rc = pthread_cond_wait(&cond, &lock);
+    }
+    Ndeliverer--;
+    rc = pthread_mutex_unlock(&lock);
+    
+    sleep(deliveryTime);
+    //Y time
+    clock_gettime(CLOCK_REALTIME,&end_timeY);
+    secondsY = (int)(end_timeY.tv_sec - start_time.tv_sec);
+    secondsCooling = (int)(end_timeY.tv_sec - start_time_cooling.tv_sec);
+    printf("Order %d has been delived in %d minutes\n",id, secondsY);
+    
+    if(counterOrder<sizeof(id)){
+        sleep(deliveryTime);
+        Ndeliverer++;
+    }else{
+        sleep(2);
+        Ndeliverer++;
+    }
+
+    //total time
+    sumTime+= secondsY;
+    coolingTime+=secondsCooling;
+
+    findMax(secondsY, &maxTime);
+    findMax(secondsCooling, &maxTimeCooling);
+    
+    
+    rc = pthread_cond_signal(&cond);
+    pthread_exit(NULL);
+}
+
+
+void findMax(int sec, int* max){
+    if(sec>*max){*max=sec;}
+}
+
+void printStatistics(void){
+    printf("\n-- Overall Statistics --\n");
+    printf("Total profit: %d\n", profit);
+    printf("Number of plain pizzas sold: %d\n", countPlain);
+    printf("Number of special pizzas sold: %d\n", countSpecial);
+    printf("Successful orders: %d\n", countSuccess);
+    printf("Failed orders: %d\n", countFail);
+    printf("Average service time : %d\n",avgTime);
+    printf("Max service time : %d\n",maxTime);
+    printf("Average cooling time : %d\n",avgTimeCooling);
+    printf("Max cooling time : %d\n",maxTimeCooling);
+}
+
+
+void acceptOrder(int id, int numberOfPizzas, int pizzas []) {
+    printf("The order %d has been accepted!\n", id);
+    countSuccess++;
+    for (int i = 0; i < numberOfPizzas; i++) {
+        double kindOfPizza = (double)rand_r(&seed) / RAND_MAX;
+        if(kindOfPizza < Pplain){
+            pizzas[i] = 0; // 0 for plain pizzas
+            profit += Cplain;
+            countPlain++;
+        } else {
+            pizzas[i] = 1; // 1 for special pizzas
+            profit += Cspecial;
+            countSpecial++;
+        }
+    }
+}
+
+
+void declineOrder(int id) {
+    printf("The order %d has been declined!\n", id);
+    countFail++;
+    pthread_exit(NULL);
 }
